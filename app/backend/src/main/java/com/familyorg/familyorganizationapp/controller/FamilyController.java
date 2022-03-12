64f3ac8b45d891849591e47a60db1ca29abe9e1f
Matt Lippelman <mattlippelman@gmail.com>
2022-03-12 05:32:39 -0600

    CU-25d7q5t Added messaging service to send emails to users

--- app/backend/src/main/java/com/familyorg/familyorganizationapp/controller/FamilyController.java
+++ app/backend/src/main/java/com/familyorg/familyorganizationapp/controller/FamilyController.java
@@ -1,10 +1,7 @@
 package com.familyorg.familyorganizationapp.controller;
 
 import java.util.List;
-
-import com.familyorg.familyorganizationapp.DTO.MemberInviteDto;
-import com.familyorg.familyorganizationapp.domain.MemberInvite;
-import com.familyorg.familyorganizationapp.service.InviteService;
+import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,160 +15,182 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.familyorg.familyorganizationapp.DTO.FamilyDto;
+import com.familyorg.familyorganizationapp.DTO.MemberInviteDto;
 import com.familyorg.familyorganizationapp.Exception.AuthorizationException;
 import com.familyorg.familyorganizationapp.Exception.BadRequestException;
 import com.familyorg.familyorganizationapp.Exception.FamilyNotFoundException;
 import com.familyorg.familyorganizationapp.Exception.UserNotFoundException;
+import com.familyorg.familyorganizationapp.domain.FamilyMembers;
+import com.familyorg.familyorganizationapp.domain.MemberInvite;
 import com.familyorg.familyorganizationapp.service.FamilyService;
+import com.familyorg.familyorganizationapp.service.InviteService;
+import com.familyorg.familyorganizationapp.service.MessagingService;
 
 @RestController
 @RequestMapping("/api/v1/family")
 public class FamilyController {
 
-	private static final Logger LOG = LoggerFactory.getLogger(FamilyController.class);
+  private static final Logger LOG = LoggerFactory.getLogger(FamilyController.class);
 
-	@Autowired
-	private FamilyService familyService;
-	@Autowired
-	private InviteService inviteService;
+  @Autowired
+  private FamilyService familyService;
+  @Autowired
+  private InviteService inviteService;
+  @Autowired
+  private MessagingService messagingService;
 
-	FamilyController() {}
+  FamilyController() {}
 
-	/**
-	 * This constructor should only be used for testing to mock the injected classes
-	 * @param familyService
-	 * @param inviteService
-	 */
-	FamilyController(FamilyService familyService, InviteService inviteService) {
-		this.familyService = familyService;
-		this.inviteService = inviteService;
-	}
+  /**
+   * This constructor should only be used for testing to mock the injected classes
+   *
+   * @param familyService
+   * @param inviteService
+   * @param messagingService
+   */
+  FamilyController(FamilyService familyService, InviteService inviteService,
+      MessagingService messagingService) {
+    this.familyService = familyService;
+    this.inviteService = inviteService;
+    this.messagingService = messagingService;
+  }
 
-	@PostMapping()
-	public ResponseEntity<?> createFamily(@RequestBody FamilyDto familyRequest) {
-		try {
-			FamilyDto createdFamily = familyService.createFamily(familyRequest);
-			return new ResponseEntity<FamilyDto>(createdFamily, HttpStatus.CREATED);
-		} catch (BadRequestException e) {
-			LOG.error(e.getMessage(), e);
-			return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
-		} catch (UserNotFoundException e) {
-			LOG.error(e.getMessage(), e);
-			return new ResponseEntity<String>(e.getMessage(), HttpStatus.NOT_FOUND);
-		} catch (Exception e) {
-			LOG.error(e.getMessage(), e);
-			return new ResponseEntity<String>("Error processing request.", HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-	}
+  @PostMapping()
+  public ResponseEntity<?> createFamily(@RequestBody FamilyDto familyRequest) {
+    try {
+      FamilyDto createdFamily = familyService.createFamily(familyRequest);
+      return new ResponseEntity<FamilyDto>(createdFamily, HttpStatus.CREATED);
+    } catch (BadRequestException e) {
+      LOG.error(e.getMessage(), e);
+      return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    } catch (UserNotFoundException e) {
+      LOG.error(e.getMessage(), e);
+      return new ResponseEntity<String>(e.getMessage(), HttpStatus.NOT_FOUND);
+    } catch (Exception e) {
+      LOG.error(e.getMessage(), e);
+      return new ResponseEntity<String>("Error processing request.",
+          HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
 
-	@PostMapping("/get-family")
-	public ResponseEntity<?> getFamily(@RequestBody FamilyDto familyRequest) {
-		try {
-			FamilyDto family = familyService.getFamily(familyRequest);
-			return new ResponseEntity<FamilyDto>(family, HttpStatus.OK);
-		} catch (FamilyNotFoundException e) {
-			LOG.error(e.getMessage(), e);
-			return new ResponseEntity<String>(e.getMessage(), HttpStatus.NOT_FOUND);
-		} catch(AuthorizationException e) {
-			LOG.error(e.getMessage(), e);
-			return new ResponseEntity<String>(e.getMessage(), HttpStatus.UNAUTHORIZED);
-		} catch(UserNotFoundException e) {
-			LOG.error(e.getMessage(), e);
-			return new ResponseEntity<String>(e.getMessage(), HttpStatus.NOT_FOUND);
-		} catch (Exception e) {
-			LOG.error(e.getMessage(), e);
-			return new ResponseEntity<String>("Error processing request.", HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-	}
+  @PostMapping("/get-family")
+  public ResponseEntity<?> getFamily(@RequestBody FamilyDto familyRequest) {
+    try {
+      FamilyDto family = familyService.getFamily(familyRequest);
+      return new ResponseEntity<FamilyDto>(family, HttpStatus.OK);
+    } catch (FamilyNotFoundException e) {
+      LOG.error(e.getMessage(), e);
+      return new ResponseEntity<String>(e.getMessage(), HttpStatus.NOT_FOUND);
+    } catch (AuthorizationException e) {
+      LOG.error(e.getMessage(), e);
+      return new ResponseEntity<String>(e.getMessage(), HttpStatus.UNAUTHORIZED);
+    } catch (UserNotFoundException e) {
+      LOG.error(e.getMessage(), e);
+      return new ResponseEntity<String>(e.getMessage(), HttpStatus.NOT_FOUND);
+    } catch (Exception e) {
+      LOG.error(e.getMessage(), e);
+      return new ResponseEntity<String>("Error processing request.",
+          HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
 
-	@GetMapping("/get-family")
-	public ResponseEntity<?> getFamilies(@RequestParam("userId") Long userId) {
-		try {
-			List<FamilyDto> families = familyService.getFamiliesByUser(userId);
-			return new ResponseEntity<List<FamilyDto>>(families, HttpStatus.OK);
-		} catch (UserNotFoundException e) {
-			LOG.error(e.getMessage(), e);
-			return new ResponseEntity<String>(e.getMessage(), HttpStatus.NOT_FOUND);
-		} catch (Exception e) {
-			LOG.error(e.getMessage(), e);
-			return new ResponseEntity<String>("Error processing request.", HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-	}
+  @GetMapping("/get-family")
+  public ResponseEntity<?> getFamilies(@RequestParam("userId") Long userId) {
+    try {
+      List<FamilyDto> families = familyService.getFamiliesByUser(userId);
+      return new ResponseEntity<List<FamilyDto>>(families, HttpStatus.OK);
+    } catch (UserNotFoundException e) {
+      LOG.error(e.getMessage(), e);
+      return new ResponseEntity<String>(e.getMessage(), HttpStatus.NOT_FOUND);
+    } catch (Exception e) {
+      LOG.error(e.getMessage(), e);
+      return new ResponseEntity<String>("Error processing request.",
+          HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
 
-	@PatchMapping()
-	public ResponseEntity<?> updateFamily(@RequestBody FamilyDto familyRequest) {
-		try {
-			FamilyDto family = familyService.updateFamily(familyRequest);
-			return new ResponseEntity<FamilyDto>(family, HttpStatus.OK);
-		} catch (AuthorizationException e) {
-			LOG.error(e.getMessage(), e);
-			return new ResponseEntity<String>(e.getMessage(), HttpStatus.UNAUTHORIZED);
-		} catch (FamilyNotFoundException e) {
-			LOG.error(e.getMessage(), e);
-			return new ResponseEntity<String>(e.getMessage(), HttpStatus.NOT_FOUND);
-		}  catch (UserNotFoundException e) {
-			LOG.error(e.getMessage(), e);
-			return new ResponseEntity<String>(e.getMessage(), HttpStatus.NOT_FOUND);
-		} catch (Exception e) {
-			LOG.error(e.getMessage(), e);
-			return new ResponseEntity<String>("Error processing request.", HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-	}
+  @PatchMapping()
+  public ResponseEntity<?> updateFamily(@RequestBody FamilyDto familyRequest) {
+    try {
+      FamilyDto family = familyService.updateFamily(familyRequest);
+      return new ResponseEntity<FamilyDto>(family, HttpStatus.OK);
+    } catch (AuthorizationException e) {
+      LOG.error(e.getMessage(), e);
+      return new ResponseEntity<String>(e.getMessage(), HttpStatus.UNAUTHORIZED);
+    } catch (FamilyNotFoundException e) {
+      LOG.error(e.getMessage(), e);
+      return new ResponseEntity<String>(e.getMessage(), HttpStatus.NOT_FOUND);
+    } catch (UserNotFoundException e) {
+      LOG.error(e.getMessage(), e);
+      return new ResponseEntity<String>(e.getMessage(), HttpStatus.NOT_FOUND);
+    } catch (Exception e) {
+      LOG.error(e.getMessage(), e);
+      return new ResponseEntity<String>("Error processing request.",
+          HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
 
-	@DeleteMapping("/delete")
-	public ResponseEntity<?> deleteFamily(@RequestParam("familyId") Long familyId) {
-		try {
-			familyService.deleteFamily(familyId);
-			return new ResponseEntity<String>("Family successfully deleted", HttpStatus.OK);
-		} catch (AuthorizationException e) {
-			LOG.error(e.getMessage(), e);
-			return new ResponseEntity<String>(e.getMessage(), HttpStatus.UNAUTHORIZED);
-		} catch (FamilyNotFoundException e) {
-			LOG.error(e.getMessage(), e);
-			return new ResponseEntity<String>(e.getMessage(), HttpStatus.NOT_FOUND);
-		} catch (UserNotFoundException e) {
-			LOG.error(e.getMessage(), e);
-			return new ResponseEntity<String>(e.getMessage(), HttpStatus.NOT_FOUND);
-		} catch (Exception e) {
-			LOG.error(e.getMessage(), e);
-			return new ResponseEntity<String>("Error processing request.", HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-	}
+  @DeleteMapping("/delete")
+  public ResponseEntity<?> deleteFamily(@RequestParam("familyId") Long familyId) {
+    try {
+      familyService.deleteFamily(familyId);
+      return new ResponseEntity<String>("Family successfully deleted", HttpStatus.OK);
+    } catch (AuthorizationException e) {
+      LOG.error(e.getMessage(), e);
+      return new ResponseEntity<String>(e.getMessage(), HttpStatus.UNAUTHORIZED);
+    } catch (FamilyNotFoundException e) {
+      LOG.error(e.getMessage(), e);
+      return new ResponseEntity<String>(e.getMessage(), HttpStatus.NOT_FOUND);
+    } catch (UserNotFoundException e) {
+      LOG.error(e.getMessage(), e);
+      return new ResponseEntity<String>(e.getMessage(), HttpStatus.NOT_FOUND);
+    } catch (Exception e) {
+      LOG.error(e.getMessage(), e);
+      return new ResponseEntity<String>("Error processing request.",
+          HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
 
-	@PostMapping("/invite/generate")
-	public ResponseEntity<?> generateInvite(@RequestBody MemberInviteDto memberInvite) {
-		LOG.warn(memberInvite.toString());
-		try {
-			if (memberInvite.isPersistent()) {
-				FamilyDto familyWithInviteCode = inviteService.generatePersistentMemberInvite(memberInvite.getFamilyId());
-				return new ResponseEntity<FamilyDto>(familyWithInviteCode, HttpStatus.OK);
-			} else {
-				// Generate the Invite
-				MemberInvite invite = null;
-				if (memberInvite.getInitialRole() != null) {
-					invite = inviteService.createUniqueMemberInviteWithRole(
-							memberInvite.getFamilyId(), memberInvite.getRecipientEmail(), memberInvite.getInitialRole());
-				} else {
-					invite = inviteService.createUniqueMemberInvite(
-							memberInvite.getFamilyId(), memberInvite.getRecipientEmail());
-				}
-				// Send the invite via email
-				// TODO: add email generation FR.6
-				LOG.info(invite.toString());
-				// Assuming no errors were thrown, an invitation was sent successfully, respond with 200 status
-				return new ResponseEntity<String>("Success", HttpStatus.OK);
-			}
-		} catch (AuthorizationException e) {
-			LOG.error(e.getMessage(), e);
-			return new ResponseEntity<String>(e.getMessage(), HttpStatus.UNAUTHORIZED);
-		} catch (FamilyNotFoundException e) {
-			LOG.error(e.getMessage(), e);
-			return new ResponseEntity<String>(e.getMessage(), HttpStatus.NOT_FOUND);
-		} catch (Exception e) {
-			LOG.error(e.getMessage(), e);
-			return new ResponseEntity<String>("Error processing request.", HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-	}
+  @PostMapping("/invite/generate")
+  public ResponseEntity<?> generateInvite(@RequestBody MemberInviteDto memberInvite) {
+    try {
+      if (memberInvite.isPersistent()) {
+        FamilyDto familyWithInviteCode =
+            inviteService.generatePersistentMemberInvite(memberInvite.getFamilyId());
+        return new ResponseEntity<FamilyDto>(familyWithInviteCode, HttpStatus.OK);
+      } else {
+        // Generate the Invite
+        MemberInvite invite = null;
+        if (memberInvite.getInitialRole() != null) {
+          invite = inviteService.createUniqueMemberInviteWithRole(memberInvite.getFamilyId(),
+              memberInvite.getRecipientEmail(), memberInvite.getInitialRole());
+        } else {
+          invite = inviteService.createUniqueMemberInvite(memberInvite.getFamilyId(),
+              memberInvite.getRecipientEmail());
+        }
+        // Send the invite via email
+        Optional<FamilyMembers> owner = invite.getFamily().getOwner();
+        if (owner.isPresent()) {
+          String emailContents = messagingService.buildInviteContent(
+              invite.getInviteCodeObj().getInviteCodeString(), owner.get().getUser().getFullname());
+          messagingService.sendHtmlEmail(invite.getUserEmail(), "You've been invited to a family!",
+              emailContents);
+        }
+        // Assuming no errors were thrown, an invitation was sent successfully, respond with 200
+        // status
+        return new ResponseEntity<String>("Success", HttpStatus.OK);
+      }
+    } catch (AuthorizationException e) {
+      LOG.error(e.getMessage(), e);
+      return new ResponseEntity<String>(e.getMessage(), HttpStatus.UNAUTHORIZED);
+    } catch (FamilyNotFoundException e) {
+      LOG.error(e.getMessage(), e);
+      return new ResponseEntity<String>(e.getMessage(), HttpStatus.NOT_FOUND);
+    } catch (Exception e) {
+      LOG.error(e.getMessage(), e);
+      return new ResponseEntity<String>("Error processing request.",
+          HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
 }
--- app/backend/src/main/java/com/familyorg/familyorganizationapp/domain/User.java
+++ app/backend/src/main/java/com/familyorg/familyorganizationapp/domain/User.java
@@ -1,11 +1,8 @@
 package com.familyorg.familyorganizationapp.domain;
 
-import org.springframework.security.core.userdetails.UserDetails;
-
 import java.io.Serializable;
 import java.util.Objects;
 import java.util.Set;
-
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -17,125 +14,144 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="users")
+@Table(name = "users")
 public class User implements Serializable {
 
-	private static final long serialVersionUID = -4364743442691665173L;
+  private static final long serialVersionUID = -4364743442691665173L;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
 
-	@Column(name="first_name", columnDefinition="VARCHAR(50)", nullable=false)
-	private String firstName;
+  @Column(name = "first_name", columnDefinition = "VARCHAR(50)", nullable = false)
+  private String firstName;
 
-	@Column(name="last_name", columnDefinition="VARCHAR(50)", nullable=false)
-	private String lastName;
+  @Column(name = "last_name", columnDefinition = "VARCHAR(50)", nullable = false)
+  private String lastName;
 
-	@Column(name="username", columnDefinition="VARCHAR(50)", nullable=false, unique=true)
-	private String username;
+  @Column(name = "username", columnDefinition = "VARCHAR(50)", nullable = false, unique = true)
+  private String username;
 
-	@Column(name="password", columnDefinition="VARCHAR(64)", nullable=false)
-	private String password;
+  @Column(name = "password", columnDefinition = "VARCHAR(64)", nullable = false)
+  private String password;
 
-	@Column(name="email", columnDefinition="VARCHAR(70)", nullable=false, unique=true)
-	private String email;
+  @Column(name = "email", columnDefinition = "VARCHAR(70)", nullable = false, unique = true)
+  private String email;
 
-	@OneToMany(mappedBy="user", fetch = FetchType.LAZY, orphanRemoval = true, cascade = CascadeType.ALL)
-	private Set<FamilyMembers> families;
-
-
-
-	public User() {}
+  @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, orphanRemoval = true,
+      cascade = CascadeType.ALL)
+  private Set<FamilyMembers> families;
 
 
-	public User(Long id, String firstName, String lastName, String username, String password, String email,
-			Set<FamilyMembers> families) {
-		this.id = id;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.username = username;
-		this.password = password;
-		this.email = email;
-		this.families = families;
-	}
 
-	public User(String firstName, String lastName, String username, String password, String email,
-				Set<FamilyMembers> families) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.username = username;
-		this.password = password;
-		this.email = email;
-		this.families = families;
-	}
+  public User() {}
 
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
 
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public Set<FamilyMembers> getFamilies() {
-		return families;
-	}
-	public void setFamilies(Set<FamilyMembers> families) {
-		this.families = families;
-	}
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", username=" + username
-				+ ", email=" + email + "]";
-	}
-	@Override
-	public int hashCode() {
-		return Objects.hash(email, firstName, id, lastName, username);
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		User other = (User) obj;
-		return Objects.equals(email, other.email)
-				&& Objects.equals(firstName, other.firstName) && Objects.equals(id, other.id)
-				&& Objects.equals(lastName, other.lastName) && Objects.equals(password, other.password)
-				&& Objects.equals(username, other.username);
-	}
+  public User(Long id, String firstName, String lastName, String username, String password,
+      String email, Set<FamilyMembers> families) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.username = username;
+    this.password = password;
+    this.email = email;
+    this.families = families;
+  }
+
+  public User(String firstName, String lastName, String username, String password, String email,
+      Set<FamilyMembers> families) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.username = username;
+    this.password = password;
+    this.email = email;
+    this.families = families;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public Set<FamilyMembers> getFamilies() {
+    return families;
+  }
+
+  public void setFamilies(Set<FamilyMembers> families) {
+    this.families = families;
+  }
+
+  public String getFullname() {
+    return this.firstName + " " + this.lastName;
+  }
+
+  @Override
+  public String toString() {
+    return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", username="
+        + username + ", email=" + email + "]";
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(email, firstName, id, lastName, username);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    User other = (User) obj;
+    return Objects.equals(email, other.email) && Objects.equals(firstName, other.firstName)
+        && Objects.equals(id, other.id) && Objects.equals(lastName, other.lastName)
+        && Objects.equals(password, other.password) && Objects.equals(username, other.username);
+  }
 
 
 
